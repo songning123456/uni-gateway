c32d8d4b975866d66c36a7404767bb0097603dd2
songning <1457065857@qq.com>
2020-04-26 16:53:10 +0800

    first commit

---
+++ src/main/java/com/uni/gateway/UniGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.uni.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniGatewayApplication.class, args);
+    }
+
+}
