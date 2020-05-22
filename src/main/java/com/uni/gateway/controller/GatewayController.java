0f0d5c4fdc02f7a0d6646bd6585ee918706d1eab
songning123456 <1457065857@qq.com>
2020-05-22 21:20:19 +0800

    eureka 修改地址

--- src/main/java/com/uni/gateway/controller/GatewayController.java
+++ src/main/java/com/uni/gateway/controller/GatewayController.java
@@ -3,6 +3,9 @@ package com.uni.gateway.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author songning
  * @date 2020/5/22
@@ -12,7 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class GatewayController {
 
     @RequestMapping(value = "/fallback")
-    public String fallback() {
-        return "fallback nothing!";
+    public Map<String, Object> doFallback() {
+        Map<String, Object> result = new HashMap<>(2);
+        result.put("status", 503);
+        result.put("message", "fallback nothing!");
+        return result;
     }
 }
